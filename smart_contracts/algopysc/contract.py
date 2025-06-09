5b730b6f8b348f93c85818136a49f28dc55454e3
Kranthi Maganti <kranthi.maganti1306@gmail.com>
2025-06-09 18:50:34 +0530

    creating account

--- smart_contracts/algopysc/contract.py
+++ smart_contracts/algopysc/contract.py
@@ -5,8 +5,16 @@ class MarketApp(ARC4Contract):
         farmer_count: UInt8
         buyer_count: UInt8
      class local_state(LocalState):
-        role: String
-        phone: String
+        role: String #'farmer or buyer'#
+        phone_no: String
         name: String
         crop_name: String
         crop_quantity: UInt8
+    @arc4.abimethod
+    def create_account(self,role:string,name:string,phone:string):
+        assert role=='farmer' or role=='buyer'
+        self.local.phone_no=phone_no
+        self.local.name=name
+        self.local.role=role
+        if role=='farmer'
+        self.global.farmer_count+=UInt8(1) #'we will increase the farmer count'#
